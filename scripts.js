0fa2164517bd6244172a77114b09ccfd7cda73af
lthomas122 <liamdanielthomas@gmail.com>
2017-02-26 22:30:34 +0000

    theme-color update

--- scripts.js
+++ scripts.js
@@ -4,12 +4,14 @@ function shakeIt(a){
 $('.menu-child').hover( function(){
   shakeIt(this);
 });
-function rgb2hex(rgb){
- rgb = rgb.match(/^rgb((d+),s*(d+),s*(d+))$/);
- return "#" +
-  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
-  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
-  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2);
+function rgb2hex(rgb) {
+    if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
+
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+        return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
 $('.menu-inner').click(function(){
   var bgCSS = $(this).css('background'),
