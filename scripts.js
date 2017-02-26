7c2a3b8705967582fb8c0d1d997e2dfb986aff39
lthomas122 <liamdanielthomas@gmail.com>
2017-02-26 16:56:08 +0000

    jquery and animate.css added

--- scripts.js
+++ scripts.js
@@ -5,10 +5,10 @@ $('.menu-child').hover( function(){
   shakeIt(this);
 });
 $('.menu-inner').click(function(){
-  var bgCSS = $(this).css('background'),
-  clCSS = $(this).css('color'),
-  choice = $(this).attr('class').split(' ')[1],
-  child = {a:'.snippets',b:'.projects',c:'.aboutme'};
+  var bgCSS = $(this).css('background');
+  var clCSS = $(this).css('color');
+  var choice = $(this).attr('class').split(' ')[1];
+  var child = {a:'.snippets',b:'.projects',c:'.aboutme'}
   $('.menu-child').hide();
   function selected(lvl,delay) {
     $('.selected').css('background', bgCSS).delay(delay).animate({right: lvl}, 500, function(){
@@ -20,21 +20,22 @@ if(delay == '120'){
   }
   selected('0','120');
   $('#Capa_1 path').css('fill', clCSS);
-  history.replaceState(null,null,child[choice]);
-  history.pushState(null, null, child[choice]);
+  //history.replaceState(null,null,child[choice]);
+  //history.pushState(null, null, child[choice]);
   $('#Capa_1, .selected .logo').click(function(e){
     $('#Capa_1, .selected .logo').hide();
     $(child[choice]).hide();
     selected('100%', '0');
     $('.menu-child').delay(750).addClass('bounceInUp').show();
-    history.replaceState(null,null,'home');
-    history.pushState(null, null,'home');
+    //history.replaceState(null,null,'home');
+    //history.pushState(null, null,'home');
   });
 });
 
 function is_touch() {
-  return 'ontouchstart' in window || navigator.maxTouchPoints;       // navigator.maxTouchPoints works on IE10/11 and Surface
-}
+  return 'ontouchstart' in window        // works on most browsers
+      || navigator.maxTouchPoints;       // works on IE10/11 and Surface
+};
 
 if(is_touch()){
   if (window.DeviceOrientationEvent){
@@ -44,15 +45,15 @@ if(is_touch()){
 window.ondeviceorientation = function(event) {
 function moveBackground2() {
 
-  var accX = (10 * event.gamma)/100,
-    accY = (10 * event.beta - 10)/100,
+  accX = (10 * event.gamma)/100;
+    accY = (10 * event.beta - 10)/100;
 		Ax = 0,
     Ay = 0,
     movement = 5;
 
     Ax += ((accX - Ax) * movement);
     Ay += ((accY - Ay) * movement);
-var  a = (Ax - Ax*2)/3,
+  a = (Ax - Ax*2)/3,
   b = (Ay - Ay*2)/3;
 
   translate = 'translate(' + Ax + 'px, ' + -Ay + 'px) scale(1.1)';
@@ -78,7 +79,7 @@ var  a = (Ax - Ax*2)/3,
  // window.requestAnimationFrame(moveBackground2);
 }
 moveBackground2();
-};
+}
 }
 else {
   var lFollowX = 0,
@@ -90,7 +91,7 @@ else {
 function moveBackground() {
   x += (lFollowX - x) * friction;
   y += (lFollowY - y) * friction;
-var  a = (x - x*2)/3,
+  a = (x - x*2)/3,
   b = (y - y*2)/3;
 
   translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
