2693eba1c932df946003bb36532ac7b32990b613
lthomas122 <liamdanielthomas@gmail.com>
2017-09-25 21:14:13 +0100

    history API fixed. AJAX loaded html added.

--- scripts.js
+++ scripts.js
@@ -15,63 +15,69 @@ function rgb2hex(rgb) {
     }
     return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
-$(function(){
-
-  var child = {a:'.snippets',b:'.projects',c:'.aboutme'};
-
-  //history.replaceState(null, null, 'home');
-
-  function selected(lvl,delay) {
-    $('.selected').css('background-color', bgCSS).delay(delay).stop().animate({right: lvl}, 500, function(){
-if(delay == '120'){
-  $('#Capa_1, .selected .logo').show();
-  $(child[choice]).show();
+function getPage(file, bar) {
+  $.get(file , function(data) {
+  $(".selected").html(data).delay(120).fadeIn();
+  document.title = "I am Lit.io | " + bar;
+  history.pushState(file, null, file);
+});
 }
-    });
-    $('meta[name=theme-color]').remove();
-    $('head').append('<meta name="theme-color" content="'+rgb2hex(bgCSS)+'">');
+function backHome() {
+  $('.menu-child').delay(750).addClass('bounceInUp');
+  $('.menu, .menu-child').show();
+  themeColor('#032D7F');
+}
+function themeColor(color){
+  $('meta[name=theme-color]').remove();
+  $('head').append('<meta name="theme-color" content="'+color+'">');
+  $('.table').css('background', color);
+}
+
+(function(){
+
+  "use strict";
+
+  var container = document.querySelector('.menu-wrapper'),
+    content = document.querySelector('.selected'),
+    defaultTitle = "I am Lit";
+  function requestContent(file, bg){
+    $.get(file , function(data) {
+    $(".selected").html(data);
+    $('.menu, .menu-child').hide();
+    themeColor(bg);
+  });
   }
 
-$('.menu-inner').click(function(e){
+  container.addEventListener('click', function(e){
+    if(e.target != e.currentTarget){
+      e.preventDefault();
+      var data = e.target.getAttribute('data-name'),
+        url = data + ".html";
+      var color = e.target.getAttribute('data-color');
+      history.pushState(data, null, url);
+      requestContent(url, color);
+      document.title = "I am Lit.io | " + data;
+    }
+    e.stopPropagation();
+  }, false);
 
-  bgCSS = $(this).css('background-color');
-  clCSS = $(this).css('color');
-  choice = $(this).attr('class').split(' ')[1];
-  url = child[choice].substr(1);
 
-  $('.menu-child').hide();
-  selected('0','120');
-  $('#Capa_1').css('color', clCSS);
-  history.pushState(child[choice], null, url);
-
-});
-
-function backHome() {
-  $('#Capa_1, .selected .logo').hide();
-  $(child[choice]).hide();
-  selected('100%', '0');
-  $('.menu-child').delay(750).addClass('bounceInUp').show();
-  $('meta[name=theme-color]').remove();
-  $('head').append('<meta name="theme-color" content="#0255a0">');
-}
-$('#Capa_1, .selected .logo').click(function(e){
-  backHome();
-  history.pushState(child[choice], null, '/');
-});
-
-window.addEventListener('popstate', function(e){
+  window.addEventListener('popstate', function(e){
     var page = e.state;
 
     if (page === null) {
+      content.innerHTML = " ";
+      document.title = defaultTitle;
       backHome();
     } else {
-      $('.menu-child').hide();
-      selected('0','120');
-      $('#Capa_1').css('color', clCSS);
+      requestContent(page + ".html");
+      document.title = "I am Lit.io | " + character;
     }
-});
+  });
+})();
 
-});
+
+
 function is_touch() {
   return 'ontouchstart' in window || navigator.maxTouchPoints;       // navigator.maxTouchPoints works on IE10/11 and Surface
 }
